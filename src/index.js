36fc1ea3acec7b48c6c10ef1dd983e38b5e6f6a5
Prabhjot Singh <pjsingh1359@gmail.com>
2022-05-10 11:28:55 -0700

    laid out current info

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import _ from 'lodash';
+/* eslint-disable linebreak-style */
 import './styles.css';
 
+function weatherLayout() {
+  const container = document.createElement('div');
+  container.classList.add('container');
+  container.innerHTML = `
+    <div class="current-weather">
+      <h1 class="current-title">Fremont,CA</h1>
+      <div class="current-icon-container">
+        <img class="current-icon" src="#" alt="weather icon">
+      </div>
+      <div class="current-temp-container">
+        <h2 class="current-temp">22F</h2>
+      </div>
+    </div>
+    `;
+  return container;
+}
+
+function searchLocation() {
+  const searchBar = document.createElement('div');
+  searchBar.classList.add('search-bar-container');
+  searchBar.innerHTML = `
+    <input type="text" class="search-bar" placeholder="Search for a city...">
+    <button class="search-button">Search</button>
+    `;
+  return searchBar;
+}
+
 function app() {
-  
+  return (
+    searchLocation(),
+    weatherLayout()
+  );
 }
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  return element;
-}
-
-document.body.appendChild(component());
+document.body.appendChild(app());
